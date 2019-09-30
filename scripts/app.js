01365dd3941c820cc2879765e3f10b57254750a6
Justin Slack <justin@justinslack.com>
2019-09-30 15:18:01 +0200

    add script

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// js Stuffs
+
+// Menu
+
+const toggleMenu = document.querySelector("#navigation button");
+const menu = document.querySelector("#navigation ul");
+
+toggleMenu.addEventListener("click", function () {
+  const open = JSON.parse(toggleMenu.getAttribute("aria-expanded"));
+  toggleMenu.setAttribute("aria-expanded", !open);
+  menu.hidden = !menu.hidden;
+});
